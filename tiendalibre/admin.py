d4b50ab35ff14b5aa8a5c4182a457bc2d2783369
Tomas <t.barragan@itsv.edu.ar>
2026-08-10 17:23:36 +0000

    feat: incorporar herencia de templates y Bootstrap

--- tiendalibre/admin.py
+++ tiendalibre/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
 from tiendalibre.models import Producto, Categoria
 
-admin.site.register(Producto)
-admin.site.register(Categoria)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'mostrar_miniatura')
+
+    readonly_fields = ('mostrar_imagen_detalle',)
+
+    def mostrar_miniatura(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="width: 50px; height: 50px; object-fit: cover; border-radius: 4px;" />', obj.imagen.url)
+        return "Sin imagen"
+
+    mostrar_miniatura.short_description = 'Miniatura'
+
+    def mostrar_imagen_detalle(self, obj):
+        if obj.imagen:
+            return format_html('<img src="{}" style="max-width: 300px; max-height: 300px; border: 1px solid #ccc;" />', obj.imagen.url)
+        return "Sin imagen"
+
+    mostrar_imagen_detalle.short_description = 'Previsualización de la Imagen'
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
